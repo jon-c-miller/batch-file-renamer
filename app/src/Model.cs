fcc6820c258b17025255bf95d3910c99a24a04eb
jon-c-miller <millerj.2203@protonmail.com>
2024-04-19 15:24:06 +0900

    working: rename model option ids enum for clarity

--- app/src/Model.cs
+++ app/src/Model.cs
@@ -2,38 +2,38 @@ namespace ConsoleFileRenamer
 {
     public class Model
     {
-        Dictionary<OptionIDs, string> displayText = new()
+        Dictionary<TextIDs, string> displayText = new()
         {
-            { OptionIDs.Header, "-- Renaming Styles --"},
-            { OptionIDs.Option1, "1. lowercase entire filename" },
-            { OptionIDs.Option2, "2. Capitalize First Letter Of Each Word In Filename" },
-            { OptionIDs.Option3, "3. UPPERCASE ENTIRE FILENAME" },
-            { OptionIDs.OptionsPrompt, "Please choose a renaming style (1 - 3): "},
-            { OptionIDs.Option1Confirm, "Set the entire filename to lowercase? " },
-            { OptionIDs.Option2Confirm, "Capitalize the first letter of each word in the filename? " },
-            { OptionIDs.Option3Confirm, "Set the entire filename to uppercase? " },
-            { OptionIDs.InvalidChoice, "is not a valid choice. "},
+            { TextIDs.Header, "-- Renaming Styles --"},
+            { TextIDs.Option1, "1. lowercase entire filename" },
+            { TextIDs.Option2, "2. Capitalize First Letter Of Each Word In Filename" },
+            { TextIDs.Option3, "3. UPPERCASE ENTIRE FILENAME" },
+            { TextIDs.OptionsPrompt, "Please choose a renaming style (1 - 3): "},
+            { TextIDs.Option1Confirm, "Set the entire filename to lowercase? " },
+            { TextIDs.Option2Confirm, "Capitalize the first letter of each word in the filename? " },
+            { TextIDs.Option3Confirm, "Set the entire filename to uppercase? " },
+            { TextIDs.InvalidChoice, "is not a valid choice. "},
         };
 
         /// <summary> Get output text that matches id. </summary>
-        public string GetDisplayText(OptionIDs textID) => displayText.ContainsKey(textID) ? displayText[textID] : "";
+        public string GetDisplayText(TextIDs textID) => displayText.ContainsKey(textID) ? displayText[textID] : "";
 
         /// <summary> Get a list of options to create the main menu. </summary>
         public string GetMainMenuText()
         {
             var sb = new System.Text.StringBuilder();
 
-            sb.Append($"{GetDisplayText(OptionIDs.Header)}");
-            sb.Append($"\n{GetDisplayText(OptionIDs.Option1)}");
-            sb.Append($"\n{GetDisplayText(OptionIDs.Option2)}");
-            sb.Append($"\n{GetDisplayText(OptionIDs.Option3)}\n");
-            sb.Append($"\n{GetDisplayText(OptionIDs.OptionsPrompt)}");
+            sb.Append($"{GetDisplayText(TextIDs.Header)}");
+            sb.Append($"\n{GetDisplayText(TextIDs.Option1)}");
+            sb.Append($"\n{GetDisplayText(TextIDs.Option2)}");
+            sb.Append($"\n{GetDisplayText(TextIDs.Option3)}\n");
+            sb.Append($"\n{GetDisplayText(TextIDs.OptionsPrompt)}");
                 
             return sb.ToString();
         }
     }
 
-    public enum OptionIDs
+    public enum TextIDs
     {
         Header,
         Option1,
