97546d9a3b43c8a296aade76385e02de9ad3a333
jon-c-miller <millerj.2203@protonmail.com>
2024-04-19 15:03:26 +0900

    working: add rudimentary logic to view to show prompt

--- app/src/View.cs
+++ app/src/View.cs
@@ -2,6 +2,17 @@ namespace ConsoleFileRenamer
 {
     public class View
     {
-        
+        public View(Model model)
+        {
+            this.model = model;
+        }
+
+        Model model;
+
+        public void Prompt()
+        {
+            Console.Clear();
+            ConsoleExtensions.PrintToConsole(model.GetMainMenuText());
+        }
     }
 }
