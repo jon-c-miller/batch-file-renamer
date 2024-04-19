f20153d6351e2a745c88cc0bfea29eb37140491c
jon-c-miller <millerj.2203@protonmail.com>
2024-04-19 15:05:01 +0900

    working: add rudimentary logic and view access to controller

--- app/src/Controller.cs
+++ app/src/Controller.cs
@@ -2,6 +2,14 @@ namespace ConsoleFileRenamer
 {
     public class Controller
     {
-        
+        public Controller()
+        {
+            view = new(model);
+        }
+
+        readonly Model model = new();
+        View view;
+
+        public void ShowPrompt() => view.Prompt();
     }
 }
