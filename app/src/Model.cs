caf3897097c128bfa2ab0b32f610880271b66bd5
jon-c-miller <millerj.2203@protonmail.com>
2024-04-19 14:44:19 +0900

    working: add a database to model to contain user-facing text

--- app/src/Model.cs
+++ app/src/Model.cs
@@ -2,6 +2,34 @@ namespace ConsoleFileRenamer
 {
     public class Model
     {
-        
+        Dictionary<OptionIDs, string> displayText = new()
+        {
+            { OptionIDs.Header, "-- Renaming Styles --"},
+            { OptionIDs.Option1, "1. lowercase entire filename" },
+            { OptionIDs.Option2, "2. Capitalize First Letter Of Each Word In Filename" },
+            { OptionIDs.Option3, "3. UPPERCASE ENTIRE FILENAME" },
+            { OptionIDs.OptionsPrompt, "Please choose a renaming style (1 - 3): "},
+            { OptionIDs.Option1Confirm, "Set the entire filename to lowercase? " },
+            { OptionIDs.Option2Confirm, "Capitalize the first letter of each word in the filename? " },
+            { OptionIDs.Option3Confirm, "Set the entire filename to uppercase? " },
+            { OptionIDs.InvalidChoice, "is not a valid choice. "},
+        };
+
+        /// <summary> Get output text that matches id. </summary>
+        public string GetDisplayText(OptionIDs textID) => displayText.ContainsKey(textID) ? displayText[textID] : "";
+
+    }
+
+    public enum OptionIDs
+    {
+        Header,
+        Option1,
+        Option2,
+        Option3,
+        OptionsPrompt,
+        Option1Confirm,
+        Option2Confirm,
+        Option3Confirm,
+        InvalidChoice,
     }
 }
