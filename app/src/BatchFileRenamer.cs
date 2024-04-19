92c8aa7021aff87612fc051e0a6f0e99927167c8
jon-c-miller <millerj.2203@protonmail.com>
2024-04-19 15:06:24 +0900

    working: add main loop to show prompt via call to controller

--- app/src/BatchFileRenamer.cs
+++ app/src/BatchFileRenamer.cs
@@ -4,7 +4,16 @@ namespace ConsoleFileRenamer
     {
         public void Run()
         {
+            Controller controller = new();
 
+            // loop until user chooses to quit
+            bool continueRenaming = true;
+            while (continueRenaming)
+            {
+                controller.ShowPrompt();
+
+                string input = Console.ReadLine();
+            }
         }
     }
 }
