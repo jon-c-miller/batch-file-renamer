778e28d1a943b5f00ecb4669978aaa885905f2af
jon-c-miller <millerj.2203@protonmail.com>
2024-04-22 17:18:46 +0900

    working: implement request handler interface in controller

--- app/src/Controller.cs
+++ app/src/Controller.cs
@@ -1,6 +1,6 @@
 namespace ConsoleFileRenamer
 {
-    public class Controller
+    public class Controller : IRequestHandler
     {
         public Controller()
         {
@@ -13,5 +13,16 @@ namespace ConsoleFileRenamer
         public void ShowPrompt() => view.Prompt();
 
         public bool HandleSelection(int choice) => view.HandleSelection(choice);
+
+        public void IRelayRequest(RequestIDs id)
+        {
+            ConsoleExtensions.PrintToConsole($"Relayed request {id}", true);
+        }
+
+        public bool IHandleRequest(RequestIDs id)
+        {
+            ConsoleExtensions.PrintToConsole($"Handled request {id}", true);
+            return true;
+        }
     }
 }
