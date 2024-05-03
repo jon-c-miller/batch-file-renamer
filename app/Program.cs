3022957dc464b40c08b4554a8b755aa069a82590
jon-c-miller <millerj.2203@protonmail.com>
2024-05-03 09:40:48 +0900

    add title to console

--- app/Program.cs
+++ app/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Title = "Batch File Renaming Utility";
+
         new BatchFileRenamer().Run();
 
         Environment.Exit(0);
